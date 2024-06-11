58736fe947a93678a77d3911dea517cd45fc8be7
nataliaangelina20 <103477003+nataliaangelina20@users.noreply.github.com>
2024-06-11 20:26:44 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
